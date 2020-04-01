06c78e10210a5b89526e44178e5950ae06509ea6
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-04-01 00:53:19 -0300

    Added UserControler and doing RecipientController

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipients';
+
+// I began here the store method to recipients. I need
+// to finish it.
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string()
+        .uppercase()
+        .required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string()
+        .required()
+        .matches(/\d{5}-\d{3}/),
+    });
+  }
+}
+
+export default new RecipientController();
