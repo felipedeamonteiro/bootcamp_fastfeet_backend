af2a53a77288868b2e7e220a57167c48a6eb89de
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-04-11 18:30:12 -0300

    Created store method in recipientController

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,28 +1,48 @@
 import * as Yup from 'yup';
 import Recipient from '../models/Recipients';
 
-// I began here the store method to recipients. I need
-// to finish it.
-
 class RecipientController {
   async store(req, res) {
-    // const schema = Yup.object().shape({
-    //   name: Yup.string().required(),
-    //   street: Yup.string().required(),
-    //   number: Yup.number().required(),
-    //   complement: Yup.string(),
-    //   state: Yup.string()
-    //     .uppercase()
-    //     .required(),
-    //   city: Yup.string().required(),
-    //   zip_code: Yup.string()
-    //     .required()
-    //     .matches(/\d{5}-\d{3}/),
-    // });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string()
+        .uppercase()
+        .required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
 
-    return res.json('Resposta ok');
+    /**
+     * zipcode Validation in front: .matches(/\d{5}-\d{3}/),
+     */
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    });
   }
-  // preciso do token pra isso rodar
 }
 
 export default new RecipientController();
