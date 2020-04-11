0bba97f144f5fdf4221f045af2007bcfbcaa7d6d
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-04-11 19:46:03 -0300

    Updated database to get if user is admin or not

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import Recipient from '../models/Recipients';
+import User from '../models/User';
 
 class RecipientController {
   async store(req, res) {
@@ -18,6 +19,17 @@ class RecipientController {
     /**
      * zipcode Validation in front: .matches(/\d{5}-\d{3}/),
      */
+    /**
+     * Check if user is admin
+     */
+    const currentUser = await User.findByPk(req.userId);
+
+    if (!currentUser.admin) {
+      return res.status(401).json({
+        error:
+          'You do not have the rights to do this. Contact the administrator.',
+      });
+    }
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation failed.' });
@@ -42,6 +54,34 @@ class RecipientController {
       city,
       zip_code,
     });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string().uppercase(),
+      city: Yup.string(),
+      zip_code: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    } = await Recipient.create(req.body);
+
+    const destiny = await Recipient.findByPk();
   }
 }
 
